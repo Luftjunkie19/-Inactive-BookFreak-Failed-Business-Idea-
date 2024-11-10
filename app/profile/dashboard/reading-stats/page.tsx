9411247a8fdcec899b6e8b980f47ac4c640fc3b7
Luftjunkie19 <kshjssjjs@gmail.com>
2024-11-10 02:00:03 +0100

    "Added state and animation to Period of Time button and modal, updated Calendar component with new props and styles"

--- app/profile/dashboard/reading-stats/page.tsx
+++ app/profile/dashboard/reading-stats/page.tsx
@@ -6,14 +6,21 @@ import Button from 'components/buttons/Button';
 import { PagesPerDayChart } from 'components/charts/competition/CompetitionCharts'
 import BaseSwiper from 'components/home/swipers/base-swiper/BaseSwiper';
 import { CalendarIcon } from 'lucide-react';
-import React from 'react'
+import React, { useState } from 'react'
 import toast from 'react-hot-toast';
+import {motion} from 'framer-motion';
 import { SwiperSlide } from 'swiper/react';
+import { DateRange } from 'react-day-picker';
+import { addDays } from 'date-fns';
 
 type Props = {}
 
 function Page({}: Props) {
-
+   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+   const [selectedDateRange, setSelectedDateRange] = useState<DateRange | undefined>({
+      from: new Date(2024, 0, 20),
+      to: addDays(new Date(2024, 0, 20), 20),
+   });
 
    const { data: document } = useQuery({
       queryKey: ['profile']
@@ -30,8 +37,14 @@ function Page({}: Props) {
 
 <div className="flex gap-2 items-center">
    <div className="relative top-0 left-0 ">
-   <Button type='blue'>Period of Time</Button>
-   <div className="absolute top-[45] -left-[160%] max-w-80 rounded-xl max-h-60 min-h-60 min-w-80 w-full h-full z-50 bg-dark-gray border-2 border-primary-color flex flex-col gap-2 p-2">
+   <Button additionalClasses='hover:bg-dark-gray hover:text-primary-color hover:scale-95 transiion-all duration-500' onClick={()=>setIsModalOpen(!isModalOpen)} type='blue'>Period of Time</Button>
+   <motion.div animate={{
+    opacity: isModalOpen ? 1 : 0,
+    scale: isModalOpen ? 1 : 0.25,
+    x: isModalOpen ? -250 : -245,
+    y: isModalOpen ? 45 : 0   ,
+    zIndex: isModalOpen ? 10 : 0
+   }} className="absolute top-0 left-0 max-w-80 rounded-xl max-h-60 min-h-60 min-w-80 w-full h-full bg-dark-gray border-2 border-primary-color flex flex-col gap-2 p-2">
       <p className="text-white">Period of Time</p>
     <div className="flex gap-2 items-center justify-between w-full">
       <p className="text-white">From</p>
@@ -40,23 +53,35 @@ function Page({}: Props) {
         <div className="flex gap-2 cursor-pointer items-center text-white bg-dark-gray py-2 px-4 h-fit w-fit rounded-lg border-2 border-primary-color"
         >
           <CalendarIcon className="mr-2 h-4 w-4" />
-           <span>Pick a date</span>
+           <span>Date Range</span>
         </div>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0" align="start">
-                <Calendar classNames={{
-                  'day_selected': 'bg-primary-color text-white',
-            
+                <Calendar
+                selected={selectedDateRange}
+                disabled={(date)=> date.getTime() >       new Date().getTime()}
+                classNames={{
+                  'day_selected': 'bg-primary-color text-white hover:bg-primary-color hover:text-white hover:scale-95 transition-all duration-300',
+                  'day_range_start':'',
+                  'day_range_middle':'',
+                  'day_range_end':'',
+                  'cell':'bg-none',
+                  'table': 'bg-dark-gray text-white',
+                  'nav_button':' text-primary-color border-primary-color hover:bg-primary-color hover:text-white hover:border-white hover:scale-95 active:scale-95 transition-all duration-300 border-2 p-1 rounded-lg',
+                  'root': 'bg-dark-gray text-white border-none', 
                   }}
           mode='range'
-                        onSelect={(day, selectedDate) => {
-                          console.log(selectedDate);
+                        onSelect={(day, selectedDate, activeModifiers) => {
+                          console.log(selectedDate, day, activeModifiers);
 
+                     
                     if (selectedDate.getTime() > new Date().getTime()) {
                       toast.error(`You cannot select dates greater than today's date.`);
                       return;
                       }
-                      console.log(selectedDate);
+                  
+                        setSelectedDateRange(day);
+                      
 
           }}
                 
@@ -65,7 +90,7 @@ function Page({}: Props) {
       </PopoverContent>
    </Popover>
     </div>
-   </div>
+   </motion.div>
    </div>
 </div>
 
