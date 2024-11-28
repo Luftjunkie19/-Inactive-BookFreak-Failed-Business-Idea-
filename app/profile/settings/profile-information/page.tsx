71ddcbf6f4f0593210ee0db60db8a057e2deef3d
Luftjunkie <kshjssjjs@gmail.com>
2024-11-28 17:18:19 +0100

    feat(profile): add hobbies selection to profile settings
    refactor: update city search functionality to handle birth and living cities separately
    
    chore(deps): remove unused dependencies from package-lock.json

--- app/profile/settings/profile-information/page.tsx
+++ app/profile/settings/profile-information/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { Popover, PopoverContent, PopoverTrigger, useDisclosure } from '@nextui-org/react';
+import { delay } from '@reduxjs/toolkit/dist/utils';
 import { useQuery } from '@tanstack/react-query';
 import Button from 'components/buttons/Button';
 import Book from 'components/elements/Book';
@@ -10,8 +11,9 @@ import { motion } from 'framer-motion';
 import { useAuthContext } from 'hooks/useAuthContext';
 import Image from 'next/image';
 import React, { Suspense, useState } from 'react'
+import { BsPersonArmsUp } from 'react-icons/bs';
 import { FaSearch } from 'react-icons/fa';
-import { FaBaby, FaBook, FaCheck } from 'react-icons/fa6';
+import { FaBaby, FaBook, FaCheck, FaMinus, FaPlus } from 'react-icons/fa6';
 import { IoIosFemale, IoIosMale } from 'react-icons/io';
 import { MdLocationCity } from 'react-icons/md';
 import { PiGenderIntersexBold } from 'react-icons/pi'
@@ -22,9 +24,15 @@ type Props = {}
 
 function ProfileInformationPage({}: Props) {
   const [selectedCity, setSelectedCity] = useState();
-  const [cityList, setCityList]=useState([]);
-  const [input, setInput]= useState('');
-  const {isOpen, onOpenChange, onOpen, onClose}=useDisclosure();
+  const [cityList, setCityList] = useState<any[]>([]);
+  const [birthCityList, setBirthCityList] = useState<any[]>([]);
+  const [input, setInput] = useState('');
+  const [openedList, setOpenedList] = useState(false);
+
+  
+
+  const { isOpen, onOpenChange, onOpen, onClose } = useDisclosure();
+  const [hobbiesSelected, setHobbiesSelected] = useState<string[]>([]);
   const {user}=useAuthContext();
 
   const {data}=useQuery({
@@ -43,10 +51,28 @@ function ProfileInformationPage({}: Props) {
     }).then((res) => res.json()),
   });
 
-  const searchForCity= async ()=>{
+  const hobbies = [
+    "Reading",
+    "Writing",
+    "Sports",
+    "Cooking",
+    "Traveling",
+    "Gardening",
+    "Art",
+    "Blogging",
+    "Photography",
+    "Crafting",
+    "Music",
+    "Gaming",
+    "Meditation",
+    "Socializing",
+    "Collecting"
+  ];
+
+  const searchForCity= async (cityFn:(param:any[])=>void)=>{
     const res= await fetch(`https://nominatim.openstreetmap.org/search?q=${input}&format=json`);
-    const cityData=await res.json();
-    setCityList(cityData);
+    const cityData = await res.json();
+    cityFn(cityData);
   };
 
   const handleCitySelect=(value)=>{
@@ -93,15 +119,17 @@ function ProfileInformationPage({}: Props) {
             <div className="flex items-center max-w-xl w-full gap-3 justify-between bg-dark-gray rounded-lg px-2 py-3">
                 <p className='text-white flex text-lg items-center gap-2'> <MdLocationCity  className='text-2xl text-primary-color' /> Living Town</p>
             <div className="flex gap-2 relative top-0 left-0 items-center">
-            <LabeledInput value={input} onChange={(e: any) => setInput(e.target.value)} type="transparent" placeholder='Living Town' />
-            <FaSearch onClick={searchForCity} size={20} className='text-primary-color cursor-pointer'/>
-           {cityList.length > 0 &&   <motion.div animate={{
-            opacity: cityList.length > 0 ? 1 : 0,
-            scale: cityList.length > 0 ? 1 : 0,
+            <LabeledInput onChange={(e: any) => setInput(e.target.value)} type="transparent" placeholder='Living Town' />
+              <FaSearch onClick={() => {
+                searchForCity(setBirthCityList);
+            }} size={20} className='text-primary-color cursor-pointer'/>
+           {birthCityList.length > 0 &&   <motion.div animate={{
+            opacity: birthCityList.length > 0 ? 1 : 0,
+            scale: birthCityList.length > 0 ? 1 : 0,
             transition: {duration: 0.5, 'bounce':0.3}
-           }} className="bg-dark-gray overflow-y-auto border-2 border-primary-color flex flex-col gap-2 absolute -bottom-52 max-w-96 p-1 min-w-80 w-full max-h-60 h-full min-h-48 rounded-lg left-0">
-              <p className='text-white text-sm'>Results: <span className='text-spotify'>{cityList.length}</span></p>
-              {cityList.map((city: any, i) => (<div onClick={()=>handleCitySelect(city.display_name)} className='text-white hover:text-primary-color transition-all cursor-pointer' key={i}>
+           }} className="bg-dark-gray  z-20 overflow-y-auto border-2 border-primary-color flex flex-col gap-2 absolute -bottom-52 max-w-96 p-1 min-w-80 w-full max-h-60 h-full min-h-48 rounded-lg left-0">
+              <p className='text-white text-sm'>Results: <span className='text-spotify'>{birthCityList.length}</span></p>
+              {birthCityList.map((city: any, i) => (<div onClick={()=>handleCitySelect(city.display_name)} className='text-white hover:text-primary-color transition-all cursor-pointer' key={i}>
             <p>{city.display_name}</p>
               </div>))}
             </motion.div>}
@@ -112,17 +140,46 @@ function ProfileInformationPage({}: Props) {
             <div className="flex items-center max-w-xl w-full gap-3 justify-between bg-dark-gray rounded-lg px-2 py-3">
                 <p className='text-white flex text-lg items-center gap-2'> <FaBaby  className='text-2xl text-primary-color' /> Birth Place</p>
             <div className="flex gap-2 relative top-0 left-0 items-center">
-            <LabeledInput value={input} onChange={(e: any) => setInput(e.target.value)} type="transparent" placeholder='Birth Place' />
-            <FaSearch onClick={searchForCity} size={20} className='text-primary-color cursor-pointer'/>
+            <LabeledInput  onChange={(e: any) => setInput(e.target.value)} type="transparent" placeholder='Birth Place' />
+              <FaSearch onClick={() => {
+                searchForCity(setCityList);
+            }} size={20} className='text-primary-color cursor-pointer'/>
            {cityList.length > 0 &&   <motion.div animate={{
             opacity: cityList.length > 0 ? 1 : 0,
             scale: cityList.length > 0 ? 1 : 0,
             transition: {duration: 0.5, 'bounce':0.3}
-           }} className="bg-dark-gray overflow-y-auto border-2 border-primary-color flex flex-col gap-2 absolute -bottom-52 max-w-96 p-1 min-w-80 w-full max-h-60 h-full min-h-48 rounded-lg left-0">
+           }} className="bg-dark-gray z-20 overflow-y-auto border-2 border-primary-color flex flex-col gap-2 absolute -bottom-52 max-w-96 p-1 min-w-80 w-full max-h-60 h-full min-h-48 rounded-lg left-0">
               <p className='text-white text-sm'>Results: <span className='text-spotify'>{cityList.length}</span></p>
               {cityList.map((city: any, i) => (<div onClick={()=>handleCitySelect(city.display_name)} className='text-white hover:text-primary-color transition-all cursor-pointer' key={i}>
             <p>{city.display_name}</p>
               </div>))}
+            </motion.div>}
+          
+            </div>
+            </div>
+
+            <div className="flex items-center max-w-xl w-full gap-3 justify-between bg-dark-gray rounded-lg px-2 py-3">
+                <p className='text-white flex text-lg items-center gap-2'> <BsPersonArmsUp  className='text-2xl text-primary-color' /> Hobbies List</p>
+            <div className="flex gap-2 relative top-0 left-0 items-center">
+           <Button onClick={()=>setOpenedList(!openedList)} type="blue">Select</Button>
+           {openedList &&   <motion.div animate={{
+            opacity: openedList ? 1 : 0,
+            scale: openedList ? 1 : 0,
+            transition: {duration: 0.5, 'bounce':0.3}
+           }} className="bg-dark-gray overflow-y-auto border-2 border-primary-color flex flex-col gap-2 absolute -bottom-52 max-w-96 p-2 min-w-80 w-full max-h-60 h-full min-h-48 rounded-lg left-0">
+                {hobbies.map((item, i) => (<div onClick={() => {
+                    if(!hobbiesSelected.find((hobby)=>item === hobby)){
+                      setHobbiesSelected([...hobbiesSelected, item])
+                    } else {
+                      setHobbiesSelected(hobbiesSelected.filter((hobby)=>item !== hobby))
+                    }
+                  }}  className={`flex cursor-pointer justify-between p-1 rounded-lg transition-all ${hobbiesSelected.find((hobby)=>item === hobby) ? "bg-green-400 hover:bg-red-400" : ""}  items-center gap-2`}>
+                  <p className='text-white font-semibold text-lg'>{item}</p>
+                  <Button type="transparent">
+                    {!hobbiesSelected.find((hobby)=>item === hobby) ? <FaPlus className='text-white'/> : <FaMinus className='text-white'/>}
+
+                  </Button>
+  </div>))}
             </motion.div>}
           
             </div>
